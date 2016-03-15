5a6da5734b53f986f6d1646bb46f026fd06d7fa6
Andrzej Michnia <amichnia@gmail.com>
2016-03-15 11:29:02 +0100

    Initial Commit

---
+++ Brain Cloud/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Brain Cloud
+//
+//  Created by Andrzej Michnia on 15/03/16.
+//  Copyright © 2016 amichnia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
