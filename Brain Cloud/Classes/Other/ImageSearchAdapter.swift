dc09ad21538eaa23130be502d0a8084720f30894
Andrzej Michnia <amichnia@gmail.com>
2016-03-18 15:56:20 +0100

    image search adapter

---
+++ Brain Cloud/Classes/Other/ImageSearchAdapter.swift
@@ -0,0 +1,28 @@
+//
+//  ImageSearchAdapter.swift
+//  Brain Cloud
+//
+//  Created by Andrzej Michnia on 18/03/16.
+//  Copyright © 2016 amichnia. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+import AlamofireSwiftyJSON
+
+enum ImagesAPI {
+    
+    static let alamofireManager = Alamofire.Manager(configuration: NSURLSessionConfiguration.ephemeralSessionConfiguration())
+    
+    static let baseUrl : String = "https://www.googleapis.com/customsearch/"
+    static let APIKey = "AIzaSyB4lSM9rrY6flfWtEhcEzgK1I5IVLSFvdQ"
+    static let CSEIdentifier = "014471330025575907481:wg54zrvhcla"
+    static let apiVersion : String = "v1"
+    
+    
+    case Search(query: String, page: Int)
+    
+    
+    
+}
