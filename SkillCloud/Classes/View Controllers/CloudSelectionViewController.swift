882449bef228430c28800cc9640c1d1a3d4fbead
Andrzej Michnia <amichnia@gmail.com>
2016-07-02 00:26:22 +0200

    Select scene
    Added scene template and graphics

---
+++ SkillCloud/Classes/View Controllers/CloudSelectionViewController.swift
@@ -0,0 +1,24 @@
+//
+//  CloudSelectionViewController.swift
+//  SkillCloud
+//
+//  Created by Andrzej Michnia on 01/07/16.
+//  Copyright © 2016 amichnia. All rights reserved.
+//
+
+import UIKit
+import PromiseKit
+
+class CloudSelectionViewController: UIViewController {
+
+    // MARK: - Outlets
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    // MARK: - Actions
+    
+    // MARK: - Navigation
+    
+}
