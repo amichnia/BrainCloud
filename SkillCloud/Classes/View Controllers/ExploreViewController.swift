e299998095b512bf57a87990907594a8cd7435d5
Andrzej Michnia <amichnia@girappe.com>
2016-07-25 14:44:39 +0200

    Template VC for explore

---
+++ SkillCloud/Classes/View Controllers/ExploreViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ExploreViewController.swift
+//  SkillCloud
+//
+//  Created by Andrzej Michnia on 25/07/16.
+//  Copyright © 2016 amichnia. All rights reserved.
+//
+
+import UIKit
+
+class ExploreViewController: UIViewController {
+
+    // MARK: - Outlets
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    // MARK: - Actions
+    
+    // MARK: - Navigation
+
+}
